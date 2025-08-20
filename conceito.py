60a176d332f16934a63369beb49ef9b41ebd7a1b
rbtavares1004 <tavaresrubinho026@gmail.com>
2025-08-20 11:59:27 -0300

    Trabalhando com if

---
+++ conceito.py
@@ -0,0 +1,24 @@
+nota1 = input("Digite a primeira nota: ")
+nota2 = input("Digite a segunda nota: ")
+nota3 = input("Digite a terceira nota: ")
+nota4 = input("Digite a quarta nota: ")
+
+# vamos converter as variáveis de notas para o tipo float
+
+nota1 = float(nota1)
+nota2 = float(nota2)
+nota3 = float(nota3)
+nota4 = float(nota4)
+
+media = (nota1 + nota2 + nota3 + nota4)  /4
+
+# Se a nota média do aluno for maior ou igual a 7, -> Aprovado
+# senão se a nota média do aluno for menor ou igual a 4, <- Reprovado
+# senão, -> Recuperação
+
+if media >= 7:
+    print("Aprovado")
+elif media <= 4:
+    print("Reprovado")
+else :
+    print("Recuperação")
